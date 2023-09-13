000210f4d8cd4823afa58f7250f79900733f3564
Ntish660 <20051742@kiit.ac.in>
2023-09-13 21:47:25 +0530

    commit

--- useeffect/src/effect.js
+++ useeffect/src/effect.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Effect = () => {
   return (
-    <div>Effect</div>
+   <>
+    <div>use of use Effect</div>
+    <div className='cointainer-fluit mt-5'>
+      <div className='row text-center'>
+        <div className='col-10 col-md-4 mt-5'>
+          
+        </div>
+      </div>
+    </div>
+   </>
   )
 }
 
